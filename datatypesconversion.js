eb9ef921d67f67e7f8105031bc7fcaed8e0597ae
Abhay rana <ranaabhayyy@gmail.com>
2024-07-21 13:15:45 +0000

    conversion of datatypes

---
+++ datatypesconversion.js
@@ -0,0 +1,19 @@
+let score= "33"
+let score1=null
+let score2="abhay"
+console.log(typeof score)
+console.log(typeof score1)
+console.log(typeof score2)
+let valuenumber= Number(score)
+let valuenumber1= Number(score1)
+let valuenumber2= Number(score2)
+console.log(typeof valuenumber)//"33"=>33
+console.log(typeof valuenumber1)//null=>0
+console.log(typeof valuenumber2)//"abhay"=>nan
+console.log(valuenumber)
+console.log(valuenumber1)
+console.log(valuenumber2)
+let somenumber= 33
+let stringnumber=String (somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)//33=>string
